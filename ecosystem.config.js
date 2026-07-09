2a94df118fb195216b35e7e1b3d1cbed87d26135
Gamit shine <shinegamit@gmail.com>
2026-07-10 05:26:06 +0530

    Pm2 Eco port 3000

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'grace-music-app',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 4001',
+      args: 'start -p 3000',
       instances: 'max', // Must be a string "max", not {max}
       exec_mode: 'cluster',
       env: {
